4e2d7d2d97eedf959379684239a0259346793e70
unknown <vladyslavvasylenko03@gmail.com>
2021-05-17 15:36:18 +0300

    new file

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
 "use strict";
 
-let setCard = document.createAttribute('div');
-
 let cliks = 0;
 
 let noShowCards = '<p>We don`t have more cards</p>';
 
 let seeMoreCards = document.getElementById("see-More-Cards");
 
+let suits = ["diamonds", "spades", "hearts", "clubs"];
+
+let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+let setCard = suits.flatMap(suit => values.map(value => `${value} of ${suit}`));
+
+//console.log(setCard);
+
 function clickCards () {
   if (clicks < 36) {
     let element = document.createElement('div');
     element.innerHTML = setCard;
-    galleries.appendChild(element);
+    see-More-Cards.appendChild(element);
     clicks++;
   } else {
     let noCards = document.createElement('p');
     noCards.innerHTML = noShowCards;
-    galleries.appendChild(noImage);
+    see-More-Cards.appendChild(noImage);
     seeMoreCards.classList.add('cards--hidden');
   }
 }
